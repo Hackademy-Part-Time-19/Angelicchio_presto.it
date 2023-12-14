78beff86e1e5c699e452d14644a580e4875e72cb
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2023-12-14 19:49:49 +0100

    miglioramento landing page

---
+++ landingPage.js
@@ -0,0 +1,6 @@
+
+window.addEventListener("load", (event) => {
+    document.getElementById("inputCategoria").value = '';
+    document.getElementById("inputPrezzo").value = '';
+    document.getElementById("inputArticolo").value = '';
+})
