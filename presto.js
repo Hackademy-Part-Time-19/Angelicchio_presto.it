3be6c1f20528223d326932b13815defb97779b5d
Maicol Angelicchio <maicol.angelicchio.91@gmail.com>
2023-12-17 12:41:57 +0100

    migliorie varie

--- presto.js
+++ presto.js
@@ -66,6 +66,13 @@ function aggiungiProdotti(filtroCategoria, filtroPrezzo, filtroArticolo){
         })
 }
 
+function cercaCatalogo(){
+    let categoria = document.getElementById("inputCategoria").value;
+    let prezzo = document.getElementById("inputPrezzo").value;
+    let articolo = document.getElementById("inputArticolo").value;
+    window.location.href = "./presto.html"+"?filtroCategoria="+categoria+"&filtroPrezzo="+prezzo+"&filtroArticolo="+articolo;
+}
+
 function dettaglioProdotto(img, nome, prezzo){
     window.location.href = "./prodotto.html"+"?articolo="+nome+"&prezzo="+prezzo+"&img="+img;
 }
@@ -76,13 +83,17 @@ window.addEventListener("load", (event) =>{
         let filtroCategoria = url.searchParams.get("filtroCategoria");
         let filtroPrezzo = url.searchParams.get("filtroPrezzo");
         let filtroArticolo = url.searchParams.get("filtroArticolo");
-
-    aggiungiProdotti(filtroCategoria, filtroPrezzo, filtroArticolo);
+        aggiungiProdotti(filtroCategoria, filtroPrezzo, filtroArticolo);
    }
    else if(window.location.href.includes("prodotto.html")){
         let url = new URL(window.location.href);
         let articolo = url.searchParams.get("articolo");
         let prezzo = url.searchParams.get("prezzo");
         let img = url.searchParams.get("img");
+   }
+   else if(window.location.href.includes("landingPage.html")){
+        document.getElementById("inputCategoria").value = '';
+        document.getElementById("inputPrezzo").value = '';
+        document.getElementById("inputArticolo").value = '';
    }
 })
